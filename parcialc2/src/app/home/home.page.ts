4d45479ca86bd00c5960c503c419fe6b13f967cb
bsbedoya <bsbedoya-2023a@corhuila.edu.co>
2025-04-22 20:25:45 -0500

    cambios

--- parcialc2/src/app/home/home.page.ts
+++ parcialc2/src/app/home/home.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FechayhoraComponent } from '../fechayhora/fechayhora.component';
 import { CommonModule } from '@angular/common';
 import {IonicModule} from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { ClienteComponent } from '../cliente/cliente.component';
 
 
 
@@ -10,8 +12,27 @@ import {IonicModule} from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [FechayhoraComponent,CommonModule,IonicModule],
+  imports: [FechayhoraComponent,CommonModule,IonicModule,FormsModule,ClienteComponent],
 })
 export class HomePage {
-  constructor() {}
+  
+  reservation = {
+    dateTime: '',
+    customer: null,
+    table: null,
+  };
+  @Input() date: string = '';
+  @Output() dateChange = new EventEmitter<string>();
+  
+  @Input() time: string = '';
+  @Output() timeChange = new EventEmitter<string>();
+   
+  onDateTimeSelected(dateTime: string) {
+    this.reservation.dateTime = dateTime;
+  }
+
+  onCustomerDataSubmitted(customer: any) {
+    this.reservation.customer = customer;
+  }
+
 }
